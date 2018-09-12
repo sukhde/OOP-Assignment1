92e266f2ca164757bfbe2c6d732c05014b4c9f12
SD18 <SD18@mitt.ca>
2018-09-12 18:37:47 -0500

    created class potion and shop

---
+++ OOP_RPG/Shop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_RPG
+{
+    class Shop
+    {
+
+        public List<Weapon> weapon{ get; set; }
+        public List<Armor> armor  { get; set; }
+        public List<potion> potion{ get; set; }
+
+    }
+}
---
+++ OOP_RPG/potion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_RPG
+{
+    public class potion
+    {
+        public int HP { get; set; }
+        public string NAME { get; set; }
+        public potion(string Name, int HP)
+        {
+            this.NAME = Name;
+            this.HP = HP;
+        }
+    }
+}
